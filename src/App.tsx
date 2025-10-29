7482e13b4b8ecd741dd009f6af54c0a4cb89dfb4
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-29 11:29:36 -0400

    updating text

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import "./App.css";
 
 const initialGrid = [
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 0
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 1
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 2
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 3
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 4
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 5
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 6
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 7
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 8
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 9
-  ["false", "false", "false", "false", "false", "false", "false", "false"], // track 10
+  [false, false, false, false, false, false, false, false], // track 0
+  [false, false, false, false, false, false, false, false], // track 1
+  [false, false, false, false, false, false, false, false], // track 2
+  [false, false, false, false, false, false, false, false], // track 3
+  [false, false, false, false, false, false, false, false], // track 4
+  [false, false, false, false, false, false, false, false], // track 5
+  [false, false, false, false, false, false, false, false], // track 6
+  [false, false, false, false, false, false, false, false], // track 7
+  [false, false, false, false, false, false, false, false], // track 8
+  [false, false, false, false, false, false, false, false], // track 9
+  [false, false, false, false, false, false, false, false], // track 10
 ];
 
 const track0 = { sound: "kick1.wav", color: "red" };
@@ -25,6 +25,7 @@ function App() {
     <>
       <div style={{ backgroundColor: "var(--background-color)", height: "100vh" }}>
         <p>test</p>
+        <div>{}</div>
       </div>
     </>
   );
